5f511ccf78144aeb0947a2cbd90956f23639a220
Sean Feldman <feldman.sean@gmail.com>
2018-12-24 13:13:05 -0700

    Approving public API Fixes #7

---
+++ src/ServiceBus.CompressionPlugin.Tests/ApiApprovals.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceBus.CompressionPlugin.Tests
+{
+    using PublicApiGenerator;
+    using ServiceBus.CompressionPlugin;
+    using Xunit;
+
+    public class ApiApprovals
+    {
+        [Fact]
+        public void CompressionPlugin()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(CompressionPlugin).Assembly,
+                whitelistedNamespacePrefixes: new[] { "Microsoft.Azure.ServiceBus." },
+                excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+
+            Approver.Verify(publicApi);
+        }
+    }
+}
