53de734d76e05c3b0059f491252b3152269f75e5
Sean Feldman <feldman.sean@gmail.com>
2019-01-02 23:30:47 -0700

    Allow multiple decompressors

--- src/ServiceBus.CompressionPlugin/CompressionConfiguration.cs
+++ src/ServiceBus.CompressionPlugin/CompressionConfiguration.cs
@@ -1,34 +1,53 @@
 ﻿namespace Microsoft.Azure.ServiceBus
 {
     using System;
+    using System.Collections.Generic;
+    using global::ServiceBus.CompressionPlugin;
 
     /// <summary>Runtime configuration for Compression plugin.</summary>
     public class CompressionConfiguration
     {
+        Dictionary<string, Func<byte[], byte[]>> mutableDecompressors;
+
+        /// <summary>
+        /// Compression configuration object to customize Compression plugin.
+        /// <remarks>Additional decompressors can be registered using AddDecompressor(name, function) API.</remarks>
+        ///  </summary>
+        /// <param name="compressionMethodName">Compression method name stored as a custom header.</param>
+        /// <param name="compressor">Function to compress an array of bytes.</param>
+        /// <param name="minimumSize">Minimum size of array for compression to be applied.</param>
+        /// <param name="decompressor">Single function to decompressor and array of bytes.</param>
+        public CompressionConfiguration(string compressionMethodName, Func<byte[], byte[]> compressor, int minimumSize, Func<byte[], byte[]> decompressor)
+            : this(compressionMethodName, compressor, minimumSize, new Dictionary<string, Func<byte[], byte[]>> { {compressionMethodName, decompressor} })
+        {
+        }
+
         /// <summary>
         /// Compression configuration object to customize Compression plugin.
         /// </summary>
         /// <param name="compressionMethodName">Compression method name stored as a custom header.</param>
         /// <param name="compressor">Function to compress an array of bytes.</param>
-        /// <param name="decompressor">Function to de-compress an array of bytes.</param>
         /// <param name="minimumSize">Minimum size of array for compression to be applied.</param>
-        public CompressionConfiguration(string compressionMethodName, Func<byte[], byte[]> compressor, Func<byte[], byte[]> decompressor, int minimumSize)
+        /// <param name="decompressors">Dictionary of function to de-compress an array of bytes where keys are compression names.</param>
+        public CompressionConfiguration(string compressionMethodName, Func<byte[], byte[]> compressor, int minimumSize, Dictionary<string, Func<byte[], byte[]>> decompressors)
         {
             Guard.AgainstEmpty(nameof(compressionMethodName), compressionMethodName);
             Guard.AgainstNull(nameof(compressor), compressor);
-            Guard.AgainstNull(nameof(decompressor), decompressor);
+            Guard.AgainstNull(nameof(decompressors), decompressors);
+            Guard.GuardAgainstEmptyCollection(nameof(decompressors), decompressors);
             Guard.AgainstNegativeOrZero(nameof(minimumSize), minimumSize);
 
-            this.CompressionMethodName = compressionMethodName;
-            this.MinimumSize = minimumSize;
-            this.Compressor = GetSafeCompressor(compressor);
-            this.Decompressor = GetSafeDecompressor(decompressor);
+            CompressionMethodName = compressionMethodName;
+            MinimumSize = minimumSize;
+            Compressor = GetSafeCompressor(compressor);
+            mutableDecompressors = decompressors;
+            Decompressors = GetSafeDecompressor(decompressors);
         }
 
         internal string CompressionMethodName { get; }
         internal int MinimumSize { get; }
         internal Func<byte[], byte[]> Compressor { get; }
-        internal Func<byte[], byte[]> Decompressor { get; }
+        internal Func<string, byte[], byte[]> Decompressors { get; }
 
         Func<byte[], byte[]> GetSafeCompressor(Func<byte[], byte[]> userProvidedCompressor)
         {
@@ -45,19 +64,34 @@
             };
         }
 
-        Func<byte[], byte[]> GetSafeDecompressor(Func<byte[], byte[]> userProvidedDecompressor)
+        Func<string, byte[], byte[]> GetSafeDecompressor(Dictionary<string, Func<byte[], byte[]>> userProvidedDecompressors)
         {
-            return bytes =>
+            return (compressionMethodName, bytes) =>
             {
+                if (!userProvidedDecompressors.TryGetValue(compressionMethodName, out var decompressor))
+                {
+                    throw new Exception($"{nameof(CompressionPlugin)} has not been configured to handle messages compressed using '{compressionMethodName}', but a message with this compression has been received. Re-configure plugin to handle '{compressionMethodName}' for decompression.");
+                }
+
                 try
                 {
-                    return userProvidedDecompressor(bytes);
+                    return decompressor(bytes);
                 }
                 catch (Exception e)
                 {
-                    throw new Exception("User provided decompression delegate threw an exception.", e);
+                    throw new Exception($"User provided decompression delegate for '{compressionMethodName}' compression method threw an exception.", e);
                 }
             };
+        }
+
+        /// <summary>
+        /// Add additional decompression method.
+        /// </summary>
+        /// <param name="compressionMethodName"></param>
+        /// <param name="decompressor"></param>
+        public void AddDecompressor(string compressionMethodName, Func<byte[], byte[]> decompressor)
+        {
+            mutableDecompressors.Add(compressionMethodName, decompressor);
         }
     }
 }
